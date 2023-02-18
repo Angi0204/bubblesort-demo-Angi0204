1bed9e1cc34dd195bd7cf59701592cbf96e88d78
Angelika <angelikaibrahim79@gmail.com>
2023-02-18 22:06:56 +0100

    Sortieren von 5 Elementen und ein Element

--- src/test/java/BubbleSortTest.java
+++ src/test/java/BubbleSortTest.java
@@ -20,7 +20,24 @@ class BubbleSortTest {
         int[] Sort = {};
         int[] expected = {};
         int[] actual = bubbleSort.sort(Sort);
-        Assertions.assertArrayEquals(expected,actual);
+        Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    void fuenfElemente() {
+        int[] Sort = {9, 8, 7, 6, 5};
+        int[] expected = {5, 6, 7, 8, 9};
+        int[] actual = bubbleSort.sort(Sort);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void einElemente() {
+        int[] Sort = {130};
+        int[] expected = {130};
+        int[] actual = bubbleSort.sort(Sort);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
 
 }
